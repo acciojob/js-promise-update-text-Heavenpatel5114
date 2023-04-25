9dae92d62b806aee09d877e24244a5422bc81bd2
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-04-25 08:53:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-
-function sayHello(){
-	return new promise((resolve) => {
-		setTimeout(()=>{
-			resolve("Hello, world!");
-		},1000);
-	});
-}
-
-const output =document.getElementbyId("output");
-sayHello().then((result)=>{
-	output.textContent=result;
-});
+let output = document.getElementById("output");
+let p = new Promise((resolve,reject) =>{
+	setInterval(resolve("Hello, world!"),1000);
+})
+p.then((value)=>{
+	output.innerHTML = value;
+})
